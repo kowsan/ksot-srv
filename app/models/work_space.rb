6b445ac642fee1144df24fecfe7e14ca6157d285
sanya <kow-san@ya.ru>
2015-07-04 21:09:50 +0300

    fix ui

--- app/models/work_space.rb
+++ app/models/work_space.rb
@@ -3,7 +3,7 @@ class WorkSpace < ActiveRecord::Base
   belongs_to :subdivision
   has_and_belongs_to_many :issue_types
 
-  validates_presence_of :name, :message => 'Имя должно быть задано'
+  validates_presence_of :name, :message => 'Наименование должно быть задано'
   validates_presence_of :subdivision_id, :message => 'Подразделение должно быть задано'
 
 
