fbf5539ac1abe9f2b064591824a2e1fca9c03057
sanya <kow-san@ya.ru>
2015-07-15 01:42:19 +0300

    date localized

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( bootstrap.css bootsrap.js glyphicons-halflings-regular.woff2 glyphicons-halflings-regular.woff glyphicons-halflings-regular.ttf   )
+
+Time::DATE_FORMATS[:ru] = '%d.%m.%Y %H:%M'
