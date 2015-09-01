a8370ff44e7e1567602b61135303f7bf049da80a
sanya <kow-san@ya.ru>
2015-09-01 21:56:56 +0300

    ukrashatelstva

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,7 +2,7 @@ class Issue < ActiveRecord::Base
   belongs_to :author, :class_name => 'User'
   belongs_to :issue_type, unscoped: true
   belongs_to :status
-  belongs_to :work_space
+  belongs_to :work_space, :unscoped => true
   belongs_to :assigned, :class_name => 'User'
   belongs_to :violator, :class_name => 'User'
   belongs_to :modifier, :class_name => 'User'
