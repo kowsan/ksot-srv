a665430043c5c3cf8427d88843d758215fc817ce
sanya <kow-san@ya.ru>
2015-09-08 23:58:07 +0300

    maxify

--- app/models/control_list_factor.rb
+++ app/models/control_list_factor.rb
@@ -1,5 +1,14 @@
 class ControlListFactor < ActiveRecord::Base
   belongs_to :control_list_factor_group
-  validates_presence_of :control_list_factor_group_id
-  validates_uniqueness_of :order_num ,:message => 'порядок сортировки уже имеется в системе'
+  validates_presence_of :control_list_factor_group_id ,:factor,:message => 'не может быть пустым'
+  validates_uniqueness_of :order_num, :message => 'порядок сортировки уже имеется в системе'
+
+  before_save :maxify
+  private
+  def maxify
+    if self.order_num.to_i==0
+      self.order_num=ControlListFactor.maximum(:order_num)+1
+    end
+  end
+
 end
--- app/models/control_list_factor_group.rb
+++ app/models/control_list_factor_group.rb
@@ -3,4 +3,13 @@ class ControlListFactorGroup < ActiveRecord::Base
   validates_uniqueness_of :order_num,:message => 'порядок сортировки уже имеется в системе'
   validates_presence_of :name,:message => 'не может быть пустым'
   default_scope {order('order_num asc')}
+
+
+  before_save :maxify
+  private
+  def maxify
+    if self.order_num.to_i==0
+      self.order_num=ControlListFactorGroup.maximum(:order_num)+1
+    end
+  end
 end
