2e0d21de50659a42ed3b965df935ce48d4213e8c
sanya <kow-san@ya.ru>
2015-09-14 23:07:14 +0300

    maxify

---
+++ db/migrate/20150825183322_create_exclusion_days.rb
@@ -0,0 +1,10 @@
+class CreateExclusionDays < ActiveRecord::Migration
+  def change
+    # create_table :exclusion_days do |t|
+    #   t.belongs_to :turn_scheme
+    #   t.belongs_to :turn_type
+    #   t.date :date
+    #   t.timestamps null: false
+    # end
+  end
+end
