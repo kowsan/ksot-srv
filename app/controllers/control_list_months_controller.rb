f9edf8aa7732d799806a2915dfcfced0de6247e3
sanya <kow-san@ya.ru>
2015-10-01 23:45:48 +0300

    staging fac disabable

--- app/controllers/control_list_months_controller.rb
+++ app/controllers/control_list_months_controller.rb
@@ -29,17 +29,20 @@ class ControlListMonthsController < ApplicationController
       @control_list_month = ControlListMonth.new(control_list_month_params)
       if !params[:factors].nil? && @control_list_month.save
         params[:factors].each_with_index do |item, index|
-          g_id, f_id=params[:factors][index].gsub('cb_', '').split('_')
-          c=ControlListMonthLink.new
-          c.control_list_month_id=@control_list_month.id
-          c.control_list_factor_group_id=g_id
-          c.control_list_factor_id=f_id
-          c.user_id=params[:f_user_id][index]
-          c.inconsistency=params[:inconsistence][index]
-          c.note_due=params[:note_due][index]
-          c.note_measures=params[:note_measures][index]
-          c.status_id=params[:f_status_id][index]
-          c.save!
+
+          if params[:is_enabled][index]
+            g_id, f_id=params[:factors][index].gsub('cb_', '').split('_')
+            c=ControlListMonthLink.new
+            c.control_list_month_id=@control_list_month.id
+            c.control_list_factor_group_id=g_id
+            c.control_list_factor_id=f_id
+            c.user_id=params[:f_user_id][index]
+            c.inconsistency=params[:inconsistence][index]
+            c.note_due=params[:note_due][index]
+            c.note_measures=params[:note_measures][index]
+            c.status_id=params[:f_status_id][index]
+            c.save!
+          end
         end
 
 
@@ -60,7 +63,29 @@ class ControlListMonthsController < ApplicationController
   # PATCH/PUT /control_list_months/1.json
   def update
     respond_to do |format|
-      if @control_list_month.update(control_list_month_params)
+      @control_list_month.update(control_list_month_params)
+      if !params[:factors].nil?
+        params[:factors].each_with_index do |item, index|
+
+          if params[:is_enabled][index]
+            g_id, f_id=params[:factors][index].gsub('cb_', '').split('_')
+            c=ControlListMonthLink.new
+            c.control_list_month_id=@control_list_month.id
+            c.control_list_factor_group_id=g_id
+            c.control_list_factor_id=f_id
+            c.user_id=params[:f_user_id][index]
+            c.inconsistency=params[:inconsistence][index]
+            c.note_due=params[:note_due][index]
+            c.note_measures=params[:note_measures][index]
+            c.status_id=params[:f_status_id][index]
+            c.save!
+          end
+        end
+
+
+      end
+
+      if @control_list_month.valid?
         format.html { redirect_to @control_list_month, notice: 'Control list month was successfully updated.' }
         format.json { render :show, status: :ok, location: @control_list_month }
       else
