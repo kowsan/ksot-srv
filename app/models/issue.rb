c6b9c66e4632727b8707a4f317b5803180cae46e
sanya <kow-san@ya.ru>
2015-10-03 20:04:21 +0300

    staging fac disabable

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,16 +1,16 @@
 class Issue < ActiveRecord::Base
-  belongs_to :author, :class_name => 'User'
+  belongs_to :author, :class_name => 'User' ,:unscoped=>true
   belongs_to :issue_type, unscoped: true
   belongs_to :status
   belongs_to :work_space, :unscoped => true
-  belongs_to :assigned, :class_name => 'User'
-  belongs_to :violator, :class_name => 'User'
-  belongs_to :modifier, :class_name => 'User'
+  belongs_to :assigned, :class_name => 'User', :unscoped => true
+  belongs_to :violator, :class_name => 'User', :unscoped => true
+  belongs_to :modifier, :class_name => 'User', :unscoped => true
   has_one :critical_type, :through => :issue_type
 
   validates_presence_of :status_id, :violator_id, :assigned_id
   validates_presence_of :work_space_id
-  validates_presence_of :issue_type_id,:message => 'не может быть пустым'
+  validates_presence_of :issue_type_id, :message => 'не может быть пустым'
   after_save :drop_redis_cache
 
 
