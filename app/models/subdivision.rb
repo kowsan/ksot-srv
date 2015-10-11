3b125efb7456a3c78c56424ba9403c7c57a06051
sanya <kow-san@ya.ru>
2015-10-11 22:56:16 +0300

    pdf beta

--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -1,10 +1,10 @@
 class Subdivision < ActiveRecord::Base
-  has_many :users
+  has_many :users, unscoped: true
   belongs_to :area
   has_many :work_spaces
   #validates_associated :area,:allow_nil => false,:allow_blank => false
   validates_presence_of :name
-  validates_presence_of :area_id,:message => 'Не могут быть пустыми'
+  validates_presence_of :area_id, :message => 'Не могут быть пустыми'
 
 
 end
