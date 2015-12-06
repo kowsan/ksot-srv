3de2b8ec618130ca1643d881f8b48f30fa0578f3
sanya <kow-san@ya.ru>
2015-12-06 16:32:17 +0300

    report ready

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,11 @@
 class ReportController < ApplicationController
   before_action :check_permission
+
   def index
+    if params[:year]
+      @date=DateTime.new(params[:year].to_i)
+    else
+      @date=DateTime.current
+    end
   end
 end
