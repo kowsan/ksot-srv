8dfc5b56c2cc0c62d19dc0912eba9775cec64655
kovalev <ak@petrosoft.su>
2015-12-29 14:36:48 +0300

    export true

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require bootstrap-datetimepicker
 
 //= require jquery.plugin
+//= require tableExport
 //= require jquery.datepick
 //= require jquery.datepick-ru
 //= require bootstrap-table
