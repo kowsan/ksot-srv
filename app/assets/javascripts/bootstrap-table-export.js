adb7a04f762e4620b05fd2bc518a954c1e1a81ae
kovalev <ak@petrosoft.su>
2015-12-29 15:03:38 +0300

    export true

--- app/assets/javascripts/bootstrap-table-export.js
+++ app/assets/javascripts/bootstrap-table-export.js
@@ -24,7 +24,7 @@
         exportDataType: 'all', // basic, all, selected
         // 'json', 'xml', 'png', 'csv', 'txt', 'sql', 'doc', 'excel', 'powerpoint', 'pdf'
        // exportTypes: ['json', 'xml', 'csv', 'txt', 'sql', 'excel'],
-        exportTypes: ['csv', 'txt', 'excel'],
+        exportTypes: ['excel'],
         exportOptions: {}
     });
 
