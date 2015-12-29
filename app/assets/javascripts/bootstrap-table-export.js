6df89fcabaa380048552b7656ec90f003b594cba
kovalev <ak@petrosoft.su>
2015-12-29 15:28:37 +0300

    export true

--- app/assets/javascripts/bootstrap-table-export.js
+++ app/assets/javascripts/bootstrap-table-export.js
@@ -38,7 +38,7 @@
 
         if (this.options.showExport) {
             var that = this,
-                $btnGroup = this.$toolbar.find('>.btn-group'),
+                $btnGroup = this.$toolbar.find('.btn-group'),
                 $export = $btnGroup.find('div.export');
 
             if (!$export.length) {
